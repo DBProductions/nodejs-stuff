9a89fd91b5c839dbda5792184a458ed585cfbd42
DBProductions <dennis@dbproductions.de>
2012-10-11 12:46:06 +0200

    move files

---
+++ filesystem/readdir-example.js
@@ -0,0 +1,27 @@
+/**
+ * simple directory iteration with display the size of the file
+ *
+ * node readdir-example.js [directory]
+ */
+
+var fs = require('fs');
+
+var dir = './';
+var args = process.argv.slice(2);
+if (args[0]) {    
+    dir = args[0];
+    if (args[0].lastIndexOf('/') !== args[0].length - 1) { dir = dir + '/'; }
+}
+
+fs.readdir(dir, function(err, files) {
+    if(err) { throw err; }
+    var countFiles = 0;
+    files.forEach(function(file) {        
+        countFiles += 1;
+        fs.stat(dir + file, function(err, stats) {
+            if (err) { throw err; }
+            console.log(file + ' (' + stats.size + ' Bytes)');
+        });        
+    });
+    console.log(countFiles + ' file in directory ' + dir);
+});
