18276d762604f448b87fb92e0f8c3c4daea83e17
DBProductions <dennis@dbproductions.de>
2012-10-28 19:18:24 +0100

    add dirty example

---
+++ database/dirty/server.js
@@ -0,0 +1,18 @@
+var db = require('dirty')('user.db');
+
+db.on('load', function() {
+	db.set('Peter', {eyes: 'blue'});
+	console.log('Added Peter, he has %s eyes.', db.get('Peter').eyes);
+	
+	db.set('bob', {eyes: 'brown'}, function() {
+		console.log('User bob is now saved on disk.');
+	});
+	
+	db.forEach(function(key, val) {
+		console.log('Found key: %s, val: %j', key, val);
+	});
+});
+
+db.on('drain', function() {
+	console.log('All records are saved on disk now');
+});
