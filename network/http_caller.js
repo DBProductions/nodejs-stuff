a7e1dfad46fdc394553a9dcc85ebf54a2028ab5b
DBProductions <dennis@dbproductions.de>
2012-11-08 18:52:48 +0100

    make some changes

--- network/http_caller.js
+++ network/http_caller.js
@@ -2,20 +2,20 @@ var http = require('http'),
     querystring = require('querystring');
 
 function httpCall(options, cb, data) {
-	var response = '';
-	var req = http.request(options, function(res) {
-		response += 'STATUS: ' + res.statusCode + "\n";
-		response += 'HEADERS: ' + JSON.stringify(res.headers);
-		res.setEncoding('utf8');
+    var response = '';
+    var req = http.request(options, function(res) {
+        response += 'STATUS: ' + res.statusCode + "\n";
+	response += 'HEADERS: ' + JSON.stringify(res.headers);
+	res.setEncoding('utf8');
     	res.on('data', function (chunk) {
-        	response += 'BODY: ' + chunk;
+            response += 'BODY: ' + chunk;
     	});
     	res.on('end', function (chunk) {
-    		cb(response);
+    	    if (cb && (typeof cb === 'function')) cb(response);
     	});
-	});
-	if (data) req.write(data);
-	req.end();
+    });
+    if (data) req.write(data);
+    req.end();
 }
 
 var options = {
@@ -32,11 +32,11 @@ var post_data = querystring.stringify({
     'email': 'user@web.com',
 });
 var options = {
-	host: 'httpbin.org',
-	port: 80,
-	path: '/post',
-	method: 'POST',
-	headers: {
+    host: 'httpbin.org',
+    port: 80,
+    path: '/post',
+    method: 'POST',
+    headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Content-Length': post_data.length
     }
