ffcf5cde60bca1f752cb89defa6689a1dcc981b4
DBProductions <dennis@dbproductions.de>
2012-11-11 21:44:06 +0100

    add a simple files example

---
+++ filesystem/files-example.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+
+fs.writeFile('filesexample.txt', 'Node works with files', function (err) {
+    if (err) throw err;
+    console.log('file saved!');
+});
+
+fs.stat('filesexample.txt', function(err, stats) {
+    if (err) throw err;
+    console.log(stats);
+});
+
+fs.readFile('filesexample.txt', 'utf-8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
+
+fs.rename("filesexample.txt", "files.txt", function (err) {
+    if (err) throw err;
+    console.log('file renamed');
+    fs.unlink('filesexample.txt');
+});
+
+fs.unlink('files.txt');
