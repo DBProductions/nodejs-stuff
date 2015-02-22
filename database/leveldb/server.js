5618e8e6ac1068062ddfb23180111d8242311e77
DBProductions <dennis@dbproductions.de>
2015-02-22 11:56:55 +0100

    add leveldb example

---
+++ database/leveldb/server.js
@@ -0,0 +1,17 @@
+var levelup = require('levelup');
+
+var db = levelup(__dirname + '/myleveldb');
+
+db.put('name', 'LevelUP', function (err) {
+    if (err) { 
+  	    console.log(err);
+  	} else {
+        db.get('name', function (err, value) {
+        	if (err) { 
+  	            console.log(err);
+  	        } else {
+                console.log('name=' + value);
+            }
+        });
+  	}
+});
