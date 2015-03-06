d6d48fb4b3d3d0ca1d0d9b86c78e9434e6d9ac77
DBProductions <dennis@dbproductions.de>
2015-03-06 23:23:39 +0100

    adjust neo4j example, still work with neo4j v2.1.7

--- database/neo4j/server.js
+++ database/neo4j/server.js
@@ -1,12 +1,14 @@
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
+console.log(db);
+
 var nodeId = null;
 var node = db.createNode({name: 'Julia'});
 
 node.save(function (err, node) {
     if (err) {
-        console.err('Error:', err);
+        console.log('Error:', err);
     } else {
         nodeId = node.id;
         console.log('Node saved:', node.id, node.data);
@@ -15,25 +17,25 @@ node.save(function (err, node) {
     var otherNode = db.createNode({name: 'Sam'});
     otherNode.save(function (err, otherNode) {
         if (err) {
-            console.err('Error:', err);
+            console.log('Error:', err);
         } else {
             console.log('Node saved:', otherNode.id, otherNode.data);
         }
 
         otherNode.createRelationshipTo(node, 'known', {status: 'classmates'}, function(err, rel) {
-            if (err) throw err;
+            if (err) { throw err; }
             console.log(otherNode.id, node.id, rel.data);        
         
             db.getNodeById(nodeId, function (err, node) {
                 console.log('Node:', node.id, node.data);
     
                 node.outgoing('known', function(err, relationships) {
-                    if (err) throw err;
+                    if (err) { throw err; }
                     console.log('outgoing:', relationships.length);
                 });
 
                 node.incoming('known', function(err, relationships) {
-                    if (err) throw err;
+                    if (err) { throw err; }
                     console.log('incoming:', relationships.length);
                 });
             });
