02a90fb7a53961e6a3b244e9daab8fd0b10c86d7
DBProductions <dennis@dbproductions.de>
2017-10-03 13:49:12 +0200

    make a better udp example

--- network/udp/client.js
+++ network/udp/client.js
@@ -1,9 +1,13 @@
-var dgram = require('dgram');
+const dgram = require('dgram');
+const client = dgram.createSocket('udp4');
 
-var client = dgram.createSocket('udp4');
+const HOST = '127.0.0.1';
+const PORT = 3000;
 
-var message = new Buffer('this is a message');
+let message = new Buffer('this is the message');
 
-client.send(message, 0, message.length, 3000, 'localhost');
-
-client.close();
+client.send(message, 0, message.length, PORT, HOST, (err, bytes) => {
+    if (err) throw err;
+    console.log(`UDP message sent to ${HOST}:${PORT}`);
+    client.close();
+});
--- network/udp/server.js
+++ network/udp/server.js
@@ -1,14 +1,16 @@
-var dgram = require('dgram');
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
 
-var server = dgram.createSocket('udp4');
+const HOST = '127.0.0.1';
+const PORT = 3000;
 
-server.on('message', function(message, rinfo) {
-	console.log('server got message: ' + message + ' from ' + rinfo.address + ':' + rinfo.port)
+server.on('message', (message, rinfo) => {
+	console.log(`server got message: ${message} from ${rinfo.address}:${rinfo.port}`)
 });
 
-server.on('listening', function() {
-	var address = server.address();
-	console.log('server listening on ' + address.address + ':' + address.port);
+server.on('listening', () => {
+	let address = server.address();
+	console.log(`server listening on ${address.address}:${address.port}`);
 });
 
-server.bind(3000);
+server.bind(PORT, HOST);
