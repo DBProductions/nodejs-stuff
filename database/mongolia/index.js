481afe9e3fa351f869f024394b4c9ca0cec35311
DBProductions <dennis@dbproductions.de>
2017-11-02 22:20:14 +0100

    adjust mongolia example

--- database/mongolia/index.js
+++ database/mongolia/index.js
@@ -1,13 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
-const format = require('util').format;
-
 const HOST = 'mongodb://127.0.0.1:27017/node';
 
 MongoClient.connect(HOST, (err, db) => {
     if(err) throw err;
 
     // create model
-    var user = require('mongolia').model(db, 'users');
+    var user = require('mongolia').model(db, 'mongolia_users');
 
     user.beforeInsert = (documents, callback) => {
 	    documents.forEach((doc) => {
@@ -16,11 +14,17 @@ MongoClient.connect(HOST, (err, db) => {
 	    callback(null, documents);
 	};
 
+    user.mongo({method: 'insert', hooks: true}, {name: 'Mongolia'}, console.log);
+
     user.mongo({method: 'insert', hooks: true}, {name: 'mongolia'}, (err, result) => {
+        if(err) throw err;
 
         console.log(result);
 
+        user.mongo({method: 'findOne', hooks: true}, {name: 'Mongolia'}, console.log);
+
         user.mongo({method: 'findArray', hooks: true}, {name: 'mongolia'}, (err, docs) => {
+            if(err) throw err;
             console.log(docs);
             // close database
             db.close();
