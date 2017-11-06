e39cb7db90cd90dc6b676c05b4b0f00b4dd92386
DBProductions <dennis@dbproductions.de>
2017-11-06 23:29:46 +0100

    combined hapi examples

--- webframeworks/hapi/server.js
+++ webframeworks/hapi/server.js
@@ -7,13 +7,35 @@ server.connection({
     port: 3000
 });
 
+server.register(require('vision'), (err) => {
+    // set views
+    server.views({
+        engines: {
+        	jade: require('pug')
+        },
+        path: __dirname + '/templates',
+        compileOptions: {
+            pretty: true
+        }
+    });
+});
+
+// define routes
+const routes = [
+    {method: 'GET', path: '/', handler: function (request, reply) { request.log(["pathData"]); reply('hapi response'); }},
+    {method: ['PUT', 'POST'], path: '/b', handler: function (request, reply) { return reply('hapi response'); }}
+];
+
 // set routes
+server.route(routes);
+
 server.route({
     method: 'GET',
-    path: '/',
+    path: '/template',
     handler: (request, reply) => {
-    	request.log(["pathData"]);
-        reply('hapi response');
+        reply.view('index', {
+            title: 'Hapi - Pug'
+        });
     }
 });
 
