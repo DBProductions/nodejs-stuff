0e75f12b50779a1b52df8ab6299e2c22542bdcf3
DBProductions <dennis@dbproductions.de>
2018-07-21 16:59:56 +0200

    adjust nats example and update package

--- messaging/nats/consumer.js
+++ messaging/nats/consumer.js
@@ -1,8 +1,35 @@
 const NATS = require('nats');
-const nats = NATS.connect();
+const nats = NATS.connect({
+  url: 'nats://localhost:4222',
+  json: true
+});
+
+nats.on('error', function(err) {
+  console.log(err);
+});
+
+nats.on('connect', function(nc) {
+  console.log('connected');
+});
+
+nats.on('disconnect', function() {
+  console.log('disconnect');
+});
+
+nats.on('reconnecting', function() {
+  console.log('reconnecting');
+});
+
+nats.on('reconnect', function(nc) {
+  console.log('reconnect');
+});
+
+nats.on('close', function() {
+  console.log('close');
+});
 
 const subject = 'subject';
 
 nats.subscribe(subject, (msg) => {
-  console.log('Received a message: ' + msg);
+  console.log(msg);
 });
--- messaging/nats/producer.js
+++ messaging/nats/producer.js
@@ -1,10 +1,37 @@
 const NATS = require('nats');
-const nats = NATS.connect();
+const nats = NATS.connect({
+  url: 'nats://localhost:4222',
+  json: true
+});
+
+nats.on('error', function(err) {
+  console.log(err);
+});
+
+nats.on('connect', function(nc) {
+  console.log('connected');
+});
+
+nats.on('disconnect', function() {
+  console.log('disconnect');
+});
+
+nats.on('reconnecting', function() {
+  console.log('reconnecting');
+});
+
+nats.on('reconnect', function(nc) {
+  console.log('reconnect');
+});
+
+nats.on('close', function() {
+  console.log('close');
+});
 
 const subject = 'subject';
-const msg = 'Hello Nats!';
+const msg = {content:{name: 'Nats'}};
 
 nats.publish(subject, msg, () => {
-  console.log('Published [' + subject + '] : "' + msg + '"');
+  console.log(subject, msg);
   nats.close();
 });
